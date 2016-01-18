72d7e8446ba947cae8b7a77ac1e1962c1c199833
jupa1089 <jpaltseva@umail.ucsb.edu>
2016-01-17 18:56:55 -0800

    make blue graph

--- citizensnorlax.R
+++ citizensnorlax.R
@@ -3,8 +3,8 @@ setwd("~/gitsnorlax/citizen-monitoring.github.io")
 pcp = read.table("pcp.txt", header=T)
 plot(pcp$meters, type="l", col="blue")
 
-boxplot(pcp$meters)
+# boxplot(pcp$meters)
 
 #git checkout master (or branch name) ((switches dropdown or working branch))
 #git merge experiment
-#git checkout -b sara (make new branch)
+#git checkout -b julia (make new branch)
