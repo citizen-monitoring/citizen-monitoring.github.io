fd3b7dc666bd1aaccf4979440d0c71ccc5858ad2
jebone <jennifer.e.bone@gmail.com>
2016-01-17 19:35:45 -0800

    Change chart title and color in experiment branch

--- testing123.R
+++ testing123.R
@@ -4,3 +4,4 @@ pcp= read.table("pcp.txt", header=T)
 pcp
 plot(pcp$meters,xlab="date", ylab="meters",col="magenta")
 
+plot(pcp$meters,ylab="rainfall (m)", main="Rainfall in Goleta, CA", col="aquamarine")
