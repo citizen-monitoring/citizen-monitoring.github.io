77b833f4c2a793b84cada43faf95a9e4b5698683
Jennifer Kidson <jen.kidson@gmail.com>
2016-01-20 14:31:56 -0800

    Delete testpush.R
    Delete duplicate in duplicate folder

--- citizen-monitoring.github.io/testpush.R
+++
@@ -1,5 +0,0 @@
-# Add pcp.txt
-
-# pcp <- read.csv("~/Dropbox/Bren Dropbox/296 Informatics/citizen-monitoring.github.io/pcp.txt", sep="") ## Code for reading in pcp data in a space delimited text file
-plot(pcp$date, pcp$meters)
-
